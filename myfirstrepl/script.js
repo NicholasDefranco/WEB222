f050a969a2ce331e30d019dd65a7c7634f9c2638
Nicholas <39540565+NicholasDefranco@users.noreply.github.com>
2020-03-19 18:25:34 -0400

    Add files via upload

---
+++ myfirstrepl/script.js
@@ -0,0 +1,12 @@
+
+var length;
+var width;
+var area;
+
+length = prompt("Enter the length");
+width = prompt("Enter the width");
+
+area = length * width;
+
+alert(`The area of the rectangle with the dimensions : 
+          ${length} and ${width} is: ${area}`);
